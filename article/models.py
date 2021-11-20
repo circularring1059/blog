38c3d9b333bbade13f79ce47b06e1f0c65fdc2c6
原件 <ring@163.com>
2021-11-20 12:21:36 +0800

    add blog

---
+++ article/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+from django.utils import timezone
+
+
+class ArticlePost(models.Model):
+    auth = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+# class article_detail(models.Model):
+#     article =
+
+
+
+
